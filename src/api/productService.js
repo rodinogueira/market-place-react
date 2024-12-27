472f83713e8298379873cb678723490985b281e8
Rodrigo Nogueira <rdg6design@gmail.com>
2024-12-27 04:51:24 -0300

    feat: add findById e edit

--- src/api/productService.js
+++ src/api/productService.js
@@ -1,3 +1,13 @@
 import api from './api'
 
-export const addProduct = (product) => api.post('/api/products', product);
+const addProduct = (product) => api.post('/api/products', product);
+const edit = (id, newProduct) => api.put(`/api/products/${id}`, newProduct);
+const findAll = () => api.get('/api/products');
+const findById = (id) => api.get(`/api/products/${id}`);
+
+export {
+    addProduct,
+    findAll,
+    edit,
+    findById
+}
