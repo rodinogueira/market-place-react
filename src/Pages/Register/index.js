75ae8e6940728c5baf184b0f9fe5cb515e11006d
Rodrigo Nogueira <44399537+rodinogueira@users.noreply.github.com>
2025-01-22 09:42:52 -0300

    fix: remove 'useContext' is defined but never used

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/logo.png'
 import { register } from '../../api/userAuth';
 import { useNavigate } from 'react-router-dom';
